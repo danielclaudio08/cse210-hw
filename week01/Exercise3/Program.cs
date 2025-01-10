31d657ab9e6937dc949f54b41aea140ad0eeeaa9
danielclaudio08 <u262229862@byui.edu>
2025-01-11 04:19:57 +0800

    week01-excercise03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,57 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Console.WriteLine("Hi! Welcome to the guess game.");
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            int numberGuess = -1;
+            int guessCount = 0;
+
+            while (numberGuess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                numberGuess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (numberGuess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (numberGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+
+            Console.WriteLine($"It took {guessCount} guesses to find the magic number!");
+            Console.WriteLine();
+
+            Console.Write("Do you want to continue? (yes/no): ");
+            response = Console.ReadLine().ToLower();
+
+            Console.WriteLine();
+
+            if (response != "yes")
+            {
+                Console.WriteLine("Thanks for playing! Goodbye.");
+            }
+
+        }
+
+
     }
+
 }
