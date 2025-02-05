21916971a4eb2bbb2bef7c289401a980777ece8d
danielclaudio08 <u262229862@byui.edu>
2025-02-06 00:13:16 +0800

    Inheritance: Add assignment classes with summary and homework list functionality

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Daniel Claudio", "Math in the Real World");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Daniel Claudio", "Geometry", "Section 9.0", "Problems 18-27");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment  = new WritingAssignment("Daniel Claudio", "Literature", "Romeo and Juliet");
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
